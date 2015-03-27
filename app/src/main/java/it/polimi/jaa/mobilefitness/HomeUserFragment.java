600d0273d1db5beef1cbb2a02f614424e4c86eea
Andre <and914@gmail.com>
2015-03-27 18:50:25 +0100

    Initial commit

---
+++ app/src/main/java/it/polimi/jaa/mobilefitness/HomeUserFragment.java
@@ -0,0 +1,45 @@
+package it.polimi.jaa.mobilefitness;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by andre on 27/03/15.
+ */
+
+public class HomeUserFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    /*public static it.polimi.jaa.mobilefitness.HomeUserFragment newInstance(int sectionNumber) {
+        it.polimi.jaa.mobilefitness.HomeUserFragment fragment = new it.polimi.jaa.mobilefitness.HomeUserFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }*/
+
+    public HomeUserFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_user_home, container, false);
+        return rootView;
+    }
+
+
+}
+
