0f2241fcf238582a0e8bdd89879024d111023346
Jaco-F <jacopo.f91@gmail.com>
2015-04-02 12:46:56 +0200

    exercises loaded from database

--- app/src/main/java/it/polimi/jaa/mobilefitness/WodFragment.java
+++ app/src/main/java/it/polimi/jaa/mobilefitness/WodFragment.java
@@ -1,15 +1,30 @@
 package it.polimi.jaa.mobilefitness;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.SyncHttpClient;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import it.polimi.jaa.mobilefitness.utils.ExerciseInfo;
@@ -21,6 +36,9 @@ public class WodFragment extends Fragment{
 
     RecyclerView recyclerView;
     int idWod;
+    private static final String PREFS = "prefs";
+    private static final String PREF_EMAIL = "email";
+    private static final String LOG_ACTIVITY = "WodFragment";
 
     public WodFragment() {
     }
@@ -36,26 +54,66 @@ public class WodFragment extends Fragment{
         recyclerView.setLayoutManager(linearLayoutManager);
 
 
-        //TODO:da togliere e mettere esericizi presi da db
-        ExerciseCardAdapter exerciseCardAdapter = new ExerciseCardAdapter(createList(10));
-        recyclerView.setAdapter(exerciseCardAdapter);
+        //TODO: METTERE IL WOD DINAMICO.....AGGIUNGERE UNA PAGINA PRIMA IN CUI SELEZIONARLO...OPPURE FORZARLO A FARNE UNO E SOLO UNO
+        SharedPreferences mSharedPreferences = this.getActivity().getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+
+        String urlServer = "http://192.168.1.187:80/wods/1/exercises";
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.setProxy("192.168.1.187",80);
+        //RequestParams requestParams = new RequestParams();
+        //requestParams.put("email", mSharedPreferences.getString(PREF_EMAIL,""));
+
+
+        client.get(urlServer,
+                new TextHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(int i, Header[] headers, String response) {
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+
+                            Log.d(LOG_ACTIVITY, jsonArray.toString());
+                            if (jsonArray.length() > 0) {
+                                //set the content of the recycler view
+                                ExerciseCardAdapter exerciseCardAdapter = new ExerciseCardAdapter(createList(jsonArray));
+                                recyclerView.setAdapter(exerciseCardAdapter);
+
+                            } else {
+                                Log.e(LOG_ACTIVITY, jsonArray.toString());
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String response, Throwable throwable) {
+                        Log.e(LOG_ACTIVITY, statusCode + throwable.getMessage());
+                    }
+                });
 
         return rootView;
     }
 
-    public void setIdWod(int idWod) {
-        this.idWod = idWod;
-    }
-
-    //TODO:togliere!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    private List<ExerciseInfo> createList(int size) {
+    private List<ExerciseInfo> createList(JSONArray jsonExercises) {
 
         List<ExerciseInfo> result = new ArrayList<ExerciseInfo>();
-        for (int i=1; i <= size; i++) {
-            ExerciseInfo ei = new ExerciseInfo("name " + i, "equipment " + i, "rounds " + i,
-                    "reps " + i, "rest " + i, "weight " + i, "time " + i,R.drawable.crossover);
-            result.add(ei);
 
+        for (int i = 0; i<jsonExercises.length();i++) {
+            try {
+                JSONObject exercise = jsonExercises.getJSONObject(i);
+
+                ExerciseInfo ei = new ExerciseInfo("name " + exercise.getString("name"), "equipment " + exercise.getString("equipment"), "rounds " + exercise.getString("rounds"),
+                        "reps " + exercise.getString("reps"), "rest " + exercise.getString("rest_time"), "weight " + exercise.getString("weight"), "time "
+                        + exercise.getString("duration"), exercise.getInt("icon_id"));
+
+                result.add(ei);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
 
         return result;
