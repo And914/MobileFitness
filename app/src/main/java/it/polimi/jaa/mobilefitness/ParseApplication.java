6a3430c80de499982154ff29425634851bad1134
Andre <and914@gmail.com>
2015-05-12 13:45:30 +0200

    added beacon

--- app/src/main/java/it/polimi/jaa/mobilefitness/ParseApplication.java
+++ app/src/main/java/it/polimi/jaa/mobilefitness/ParseApplication.java
@@ -2,6 +2,7 @@ package it.polimi.jaa.mobilefitness;
 
 
 import android.app.Application;
+import android.util.Log;
 
 import com.facebook.FacebookSdk;
 import com.parse.Parse;
@@ -9,11 +10,36 @@ import com.parse.ParseACL;
 import com.parse.ParseCrashReporting;
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
+import com.gimbal.android.Gimbal;
+
+
+import com.gimbal.android.PlaceManager;
+import com.gimbal.android.PlaceEventListener;
+import com.gimbal.android.Place;
+import com.gimbal.android.Visit;
+
+import com.gimbal.android.CommunicationManager;
+import com.gimbal.android.CommunicationListener;
+import com.gimbal.android.Communication;
+import com.gimbal.android.Push;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import com.gimbal.android.BeaconEventListener;
+import com.gimbal.android.BeaconManager;
+import com.gimbal.android.BeaconSighting;
 
 /**
  * Created by Jacopo on 05/05/2015.
  */
 public class ParseApplication extends Application {
+    private PlaceEventListener placeEventListener;
+    private CommunicationListener communicationListener;
+    private BeaconEventListener beaconSightingListener;
+    private BeaconManager beaconManager;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -34,5 +60,64 @@ public class ParseApplication extends Application {
 
         FacebookSdk.sdkInitialize(getApplicationContext());
         ParseFacebookUtils.initialize(getApplicationContext());
+
+        Gimbal.setApiKey(this, getString(R.string.gimbal_key));
+
+
+
+        placeEventListener = new PlaceEventListener() {
+            @Override
+            public void onVisitStart(Visit visit) {
+                // This will be invoked when a place is entered. Example below shows a simple log upon enter
+                Log.i("Info:", "Enter: " + visit.getPlace().getName() + ", at: " + new Date(visit.getArrivalTimeInMillis()));
+            }
+
+            @Override
+            public void onVisitEnd(Visit visit) {
+                // This will be invoked when a place is exited. Example below shows a simple log upon exit
+                Log.i("Info:", "Exit: " + visit.getPlace().getName() + ", at: " + new Date(visit.getDepartureTimeInMillis()));
+            }
+        };
+        PlaceManager.getInstance().addListener(placeEventListener);
+
+
+        communicationListener = new CommunicationListener() {
+            @Override
+            public Collection<Communication> presentNotificationForCommunications(Collection<Communication> communications, Visit visit) {
+                for (Communication comm : communications) {
+                    Log.i("INFO", "Place Communication: " + visit.getPlace().getName() + ", message: " + comm.getTitle());
+                }
+                //allow Gimbal to show the notification for all communications
+                return communications;
+            }
+
+            @Override
+            public Collection<Communication> presentNotificationForCommunications(Collection<Communication> communications, Push push) {
+                for (Communication comm : communications) {
+                    Log.i("INFO", "Received a Push Communication with message: " + comm.getTitle());
+                }
+                //allow Gimbal to show the notification for all communications
+                return communications;
+            }
+
+            @Override
+            public void onNotificationClicked(List<Communication> communications) {
+                Log.i("INFO", "Notification was clicked on");
+            }
+        };
+        CommunicationManager.getInstance().addListener(communicationListener);
+
+        beaconSightingListener = new BeaconEventListener() {
+            @Override
+            public void onBeaconSighting(BeaconSighting sighting) {
+                Log.i("INFO", sighting.toString());
+            }
+        };
+        beaconManager = new BeaconManager();
+        beaconManager.addListener(beaconSightingListener);
+
+        PlaceManager.getInstance().startMonitoring();
+        beaconManager.startListening();
+        CommunicationManager.getInstance().startReceivingCommunications();
     }
 }
