889abcb9e186249d4c49df86967f9da6610201ae
Andre <and914@gmail.com>
2015-06-21 17:15:06 +0200

    UI changes

--- app/src/main/java/it/polimi/jaa/mobilefitness/SendResultsNFCActivity.java
+++ app/src/main/java/it/polimi/jaa/mobilefitness/SendResultsNFCActivity.java
@@ -6,6 +6,7 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -15,6 +16,7 @@ import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,6 +35,7 @@ public class SendResultsNFCActivity extends ActionBarActivity implements NfcAdap
 
     private TextView resultText;
     private TextView opponentResultText;
+    private ImageView imageResult;
 
     private int result;
     private int opponentResult;
@@ -67,6 +70,10 @@ public class SendResultsNFCActivity extends ActionBarActivity implements NfcAdap
         resultText = (TextView) findViewById(R.id.your_score);
         opponentResultText = (TextView) findViewById(R.id.opponent_score);
         resultText.setText(String.valueOf(result));
+
+        imageResult = (ImageView) findViewById(R.id.image_result);
+        imageResult.setImageResource(R.drawable.ic_don_t_give_up);
+
         /*senderNFCButton = (Button) findViewById(R.id.button_sender_nfc);
 
         senderNFCButton.setOnClickListener(new View.OnClickListener() {
@@ -148,6 +155,14 @@ public class SendResultsNFCActivity extends ActionBarActivity implements NfcAdap
             if(!mFirst) {
                 SendBackDialogFragment sendBackDialogFragment = new SendBackDialogFragment();
                 sendBackDialogFragment.show(getFragmentManager(), "send_back_dialog");
+            }
+            if (opponentResult > result){
+                opponentResultText.setTypeface(null, Typeface.BOLD);
+                imageResult.setImageResource(R.drawable.ic_don_t_give_up);
+
+            } else {
+                resultText.setTypeface(null, Typeface.BOLD);
+                imageResult.setImageResource(R.drawable.ic_3256270_winner);
             }
         } catch (JSONException e) {
             e.printStackTrace();
